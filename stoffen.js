dad278c18b6acebad715168f526ae8a3e38fd08e
DeOostBespokeTailoring <samson@deoost.nl>
2026-07-14 13:58:07 +0200

    Add files via upload

--- stoffen.js
+++ stoffen.js
@@ -8,6 +8,38 @@
 window.DEOST_STOFFEN = [
 
   {
+    id: "STANDEVEN-OXBRIDGE-FLANNEL-22000",
+    leverancier: "Standeven",
+    bunch: "Oxbridge Flannel 1",
+    stofnummer: "22000",
+    naam: "Standeven Oxbridge Flannel 22000",
+    materiaal: ["wol"],
+    samenstelling: "100% wol",
+    weving: "flanel",
+    patroon: "effen",
+    kleur: "grijs",
+    patroonkleur: "",
+    gewicht: "",
+    prijsGroep: "C",
+    geschiktVoor: ["pak_kostuum", "tenue_de_ville", "los_jasje", "losse_broek"],
+    gebruik: ["zakelijk", "bruiloft", "winter", "casual"],
+    seizoen: ["herfst", "winter"],
+    formaliteit: ["semi-formeel", "formeel"],
+    uitstraling: ["klassiek", "zacht", "rustig"],
+    klantLabels: ["Flanel", "Winter", "Zacht"],
+    korteUitleg: "Een klassieke flanel uit Standeven Oxbridge Flannel met een zachte, matte uitstraling.",
+    advies: "Sterk als winterpak, losse broek of klassiek tenue de ville onderdeel.",
+    minderGeschiktVoor: "Minder geschikt voor warm weer of een zeer glad/formeel avondbeeld.",
+    swatch: "stoffen/Standeven/Oxbridge Flannel 1/22000_swatch.jpg",
+    closeup: "stoffen/Standeven/Oxbridge Flannel 1/22000_closeup.jpg",
+    video: "stoffen/Standeven/Oxbridge Flannel 1/22000_light.mp4",
+    seoUrl: "",
+    blogUrl: "",
+    videoUrl: "",
+    actief: true
+  },
+
+  {
     id: "MOCK-NAVY-HOPSACK",
     leverancier: "De Oost Mock Library",
     bunch: "Demo Suiting",
